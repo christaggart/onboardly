1b0939b5f817a15946f87e4c57a92f3da37a39ac
Chris <chris@thinkit.ca>
2012-11-13 01:27:32 -0500

    Uncommented some things. Removed flint install

--- onboard.sh
+++ onboard.sh
@@ -23,7 +23,7 @@ cat > soloistrc <<EOF
 cookbook_paths:
 - $PWD
 recipes:
-#- pivotal_workstation::alfred
+- pivotal_workstation::alfred
 - pivotal_workstation::bash_path_order
 - pivotal_workstation::bash_profile-aliases
 - pivotal_workstation::bash_profile-better_history
@@ -31,30 +31,29 @@ recipes:
 - pivotal_workstation::bash_profile-git_completion
 - pivotal_workstation::bash_profile-no_sudo_gem_install
 - pivotal_workstation::bash_profile-ps1
-#- pivotal_workstation::chrome
+- pivotal_workstation::chrome
 - pivotal_workstation::create_var_chef_cache
 - pivotal_workstation::defaults_fast_key_repeat_rate
-#- pivotal_workstation::dropbox
-#- pivotal_workstation::firefox
+- pivotal_workstation::dropbox
+- pivotal_workstation::firefox
 - pivotal_workstation::flycut
 - pivotal_workstation::function_keys
 - pivotal_workstation::gem_no_rdoc_no_ri
-#- pivotal_workstation::git
-#- pivotal_workstation::github_for_mac
-#- pivotal_workstation::git_config_global_defaults
+- pivotal_workstation::git
+- pivotal_workstation::github_for_mac
+- pivotal_workstation::git_config_global_defaults
 - pivotal_workstation::global_environment_variables
-#- pivotal_workstation::homebrew
+- pivotal_workstation::homebrew
 - pivotal_workstation::increase_shared_memory
-#- pivotal_workstation::inputrc
+- pivotal_workstation::inputrc
 - pivotal_workstation::iterm2
-#- pivotal_workstation::keyboard_preferences
-#- pivotal_workstation::locate_on
+- pivotal_workstation::keyboard_preferences
+- pivotal_workstation::locate_on
 - pivotal_workstation::menubar_preferences
 - pivotal_workstation::osx_updates
-#- pivotal_workstation::propane
+#### todo: swap this for flint trial - pivotal_workstation::propane
 - pivotal_workstation::rvm
-#- pivotal_workstation::sshd_on
-#- pivotal_workstation::unix_essentials
+- pivotal_workstation::unix_essentials
 - pivotal_workstation::user_owns_usr_local
 - pivotal_workstation::vagrant
 - pivotal_workstation::vim
